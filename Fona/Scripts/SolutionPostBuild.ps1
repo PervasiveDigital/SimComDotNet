d85501c602952dba81db4b8e382bec26f0aade7e
Martin Calsyn <martin@prometheusengineering.net>
2015-04-05 18:09:16 +0300

    Support nuget symbols server

--- Fona/Scripts/SolutionPostBuild.ps1
+++ Fona/Scripts/SolutionPostBuild.ps1
@@ -19,12 +19,15 @@ if ($nuspec)
 
 	$nugetBuildDir = $SolutionDir + "nuget\" + $ConfigurationName + "\" + $ProjectName + "\"
 	$libDir = $nugetBuildDir + "lib\"
+	$srcDir = $nugetBuildDir + "src\"
 
 	Write-Verbose "Nuget build dir is $nugetBuildDir"
 	$nuget = $SolutionDir + ".nuget\nuget.exe"
 
 	if (test-path $nugetBuildDir) { ri -r -fo $nugetBuildDir }
 	mkdir $libDir | out-null
+	mkdir $srcDir | out-null
+
 	if ($IsNetMF)
 	{
 		mkdir $libDir"\netmf\be" | out-null
@@ -39,9 +42,25 @@ if ($nuspec)
 		Copy-Item -Path $TargetDir"*" -Destination $libDir"\net40" -Include "*.dll","*.pdb","*.xml"
 	}
 
+	# Copy source files
+#	$source = $SolutionDir + "Fona.Shared"
+#	Get-ChildItem $source -Recurse -Filter "*.cs" | Copy-Item -Destination {Join-Path $srcDir $_.FullName.Substring($source.length)}
+#	$source = $ProjectDir
+#	Get-ChildItem $source -Recurse -Filter "*.cs" -Exclude "obj","bin" | Copy-Item -Destination {Join-Path $srcDir $_.FullName.Substring($source.length)}
+
+	Copy-Item -Recurse -Path $SolutionDir"Fona.Shared" -Destination $srcDir -Filter "*.cs"
+	$target = $srcDir + "Fona.Shared"
+	if (test-path $target"\obj") { Remove-Item -Recurse $target"\obj" | out-null }
+	if (test-path $target"\bin") { Remove-Item -Recurse $target"\bin" | out-null }
+	
+	Copy-Item -Recurse -Path $ProjectDir -Destination $srcDir -Filter "*.cs"
+	$target = $srcDir + $ProjectName
+	if (test-path $target"\obj") { Remove-Item -Recurse $target"\obj" | out-null }
+	if (test-path $target"\bin") { Remove-Item -Recurse $target"\bin" | out-null }
+
 	$output = $repoDir + $ConfigurationName
 	if (-not (test-path $output)) { mkdir $output | out-null }
 
-	$args = 'pack', $nuspec, '-basepath', $nugetBuildDir, '-OutputDirectory', $output
+	$args = 'pack', $nuspec, '-Symbols', '-basepath', $nugetBuildDir, '-OutputDirectory', $output
 	& $nuget $args
 }
